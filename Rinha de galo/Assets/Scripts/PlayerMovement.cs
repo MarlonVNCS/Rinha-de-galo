7a8dc0cb46c62803dad99f09f35e5747b0e9229c
MarlonVNCS <marlon-vinicius@live.com>
2020-07-28 23:31:47 -0300

    Primeiro upload
    Implementado:
    Primeiro modelo de galo,
    Movimentação(incompleta)

---
+++ Rinha de galo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveSpeed = 10;
+    float jumpForce = 1F;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis ("Horizontal"); 
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        float jumpInput = Input.GetAxis("Jump");
+
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime);
+        if(Input.GetKeyDown("space"))
+            transform.Translate(new Vector3(0, jumpInput, 0) * jumpForce);
+
+    }
+}
